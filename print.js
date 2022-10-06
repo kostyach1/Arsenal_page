e5a75f73e2f2bbbb8d27da30f92781a4d7240324
kostyach1 <57063701+kostyach1@users.noreply.github.com>
2022-10-06 15:39:46 +0300

    Add files via upload

---
+++ print.js
@@ -0,0 +1,8 @@
+		"use strict";
+		function print_() {		
+			var x=10;
+			alert('******************\r\n***                  ***\r\n***  !HELLO!   ***\r\n***                  ***\r\n******************');
+			document.write(x);
+			console.log('запись в консоль');
+			}
+		print_()
